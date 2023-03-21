69514636fdd18fef0b41b2b25f293f4d0d91170d
Marc Bodmer <marc.bodmer@gmail.com>
2023-03-20 21:47:44 -0700

    Add Posthog snippet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,36 @@
 import "../styles/globals.css";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Disable in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.opt_out_capturing();
+    },
+  });
+}
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  );
 }
